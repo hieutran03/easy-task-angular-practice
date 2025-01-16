10522ea6d0075e72fba497d6826f6661c08c7571
@hieutran03 <hieumdg@gmail.com>
2025-01-16 21:50:00 +0700

    Create a Configurable Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId!: string;
+
+  get selectedUser(){
+    const user = this.users.find((user)=>user.id === this.selectedUserId);
+    if(!user)
+      throw new Error("No user is found");
+    return user;
+  }
 
   onSelectedUser(id: string){
-    console.log("Selected user with id " + id)
+    this.selectedUserId = id; 
   }
 }

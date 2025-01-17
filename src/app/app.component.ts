bd2b0671f4b4da86670ad42e4c1de6a3caee9c14
@hieutran03 <hieumdg@gmail.com>
2025-01-17 09:55:32 +0700

    tweak a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from './task/task.component';
+import { TaskComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,8 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'app-task',
   standalone: true,
   imports: [],
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css',
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) name?: string;
